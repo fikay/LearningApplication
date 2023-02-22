008930124422ecb6a1972868b955c0b269ccb0b6
Fikayo Oluwakeye <fikayofakeye@yahoo.com>
2023-02-22 14:38:35 -0600

    Created Database Connection to WPF Application and showed list of items from the database and associated Items

--- WpfApp Zoo Manger/MainWindow.xaml.cs
+++ WpfApp Zoo Manger/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 namespace WpfApp_Zoo_Manger
 {
     /// <summary>
@@ -20,9 +22,69 @@ namespace WpfApp_Zoo_Manger
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
         public MainWindow()
         {
             InitializeComponent();
+            string connectionstring = ConfigurationManager.ConnectionStrings["WpfApp_Zoo_Manger.Properties.Settings.FikayoDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionstring);
+            ShowZoos();
+            
+           
+        }
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+                    Zoolist.DisplayMemberPath = "Location";
+                    Zoolist.SelectedValuePath = "Id";
+                    Zoolist.ItemsSource = zooTable.DefaultView;
+
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString()); 
+            }
+        }
+
+        private void ShowAnimal()
+        {
+            try
+            {
+                string Query = "Select * from Animal a inner join ZooAnimal za on a.id = za.AnimalId where za.ZooId = @ZooId ";
+                SqlCommand sqlCommand = new SqlCommand(Query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                using(sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@ZooId",Zoolist.SelectedValue);
+                    DataTable animalTable = new DataTable();
+                    sqlDataAdapter.Fill(animalTable);
+
+                    Animallist.DisplayMemberPath = "Name";
+                    Animallist.SelectedValuePath = "Id";
+                    Animallist.ItemsSource = animalTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Zoolist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAnimal();
+
         }
     }
 }
--- WpfApp Zoo Manger/Properties/Settings.Designer.cs
+++ WpfApp Zoo Manger/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApp_Zoo_Manger.Properties
-{
-
-
+namespace WpfApp_Zoo_Manger.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=FAKS\\FIKAYOMYSQL;Initial Catalog=FikayoDB;Persist Security Info=True;" +
+            "User ID=sa;Password=Fik@yo8243!")]
+        public string FikayoDBConnectionString {
+            get {
+                return ((string)(this["FikayoDBConnectionString"]));
             }
         }
     }
